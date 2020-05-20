4db6dbb5b77c339b9b4177d9f8a6ee35941f706c
MaksumNechiporuk <maksum.nec@gmail.com>
2020-05-20 19:46:07 +0200

    change loop type

--- HomeWork/1/1.4/index.php
+++ HomeWork/1/1.4/index.php
@@ -1,8 +1,10 @@
 <?php
 $arr = [1, 6, 3, 8, 66, 4, 99, 6, 34, 78];
 $sum = 0;
-foreach ($arr as $value) {
-    $sum += $value;
+$i = 0;
+while ($i < count($arr)) {
+    $sum += $arr[$i];
+    $i++;
 }
 echo "Sum=" . $sum . "<br>";
 echo "Arithmetic mean=" . $sum / count($arr);
